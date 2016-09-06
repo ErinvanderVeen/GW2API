39b0798ca8721383a5820c91a647be01d5d6d0c6
Erin van der Veen <erinvanderveen@outlook.com>
2016-09-06 17:17:06 +0200

    Implement account/dyes endpoint

---
+++ GW2API/Source/Account/Dyes.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using GW2API.Core;
+
+namespace GW2API.Account.Dyes
+{
+	public class Dyes
+	{
+		private GW2API<List<int>> gw2api;
+
+		public List<int> dyes { get; set; }
+
+		public Dyes (string apiKey)
+		{
+			this.dyes = new List<int> ();
+			this.gw2api = new GW2API<List<int>> (dyes, "account/dyes", apiKey);
+		}
+
+		public void Update ()
+		{
+			gw2api.Download ();
+		}
+	}
+}
